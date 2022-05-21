9d58acc1b9fcaf916a50e95ed430beef2727fb80
Edson Michaque <edson@michaque.com>
2022-05-22 01:02:56 +0200

    again, try better names

---
+++ internal/server/operator/handler.go
@@ -0,0 +1,25 @@
+package operator
+
+import (
+	"net/http"
+
+	"github.com/edsonmichaque/omni/internal"
+)
+
+type Handler struct {
+	internal.Handler
+	Cache  internal.Cache
+	Logger internal.Logger
+}
+
+func (h Handler) SendMessages(rw http.ResponseWriter, r *http.Request) {
+	var req NewSMS
+
+	if err := h.ReadJSON(r.Body, &req); err != nil {
+		return
+	}
+}
+
+func (h Handler) ListMessages(rw http.ResponseWriter, r *http.Request) {}
+
+func (h Handler) GetMessage(rw http.ResponseWriter, r *http.Request) {}
