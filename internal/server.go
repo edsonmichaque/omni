0e6e83673219bae14943341efb69c187841342ca
Edson Michaque <edson@michaque.com>
2022-05-21 23:36:24 +0200

    add config type

--- internal/server.go
+++ internal/server.go
@@ -1,3 +1,32 @@
 package internal
 
-type Server struct{}
+import (
+	"net/http"
+
+	"github.com/edsonmichaque/omni/internal/sms"
+	"github.com/edsonmichaque/omni/internal/ussd"
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	smsHandler  sms.Handler
+	ussdHandler ussd.Handler
+}
+
+func (s Server) router() *mux.Router {
+	r := mux.NewRouter()
+
+	return r
+}
+
+func (s Server) start() error {
+	srv := http.Server{
+		Handler: s.router(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
