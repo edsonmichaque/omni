b08912c46b44a85577a2048cb5e2e835702d6916
Edson Michaque <edson@michaque.com>
2022-05-21 22:44:23 +0200

    add message handler

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/edsonmichaque/omni
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
+
+replace github.com/edsonmichaque/omni => ./
---
+++ internal/apiserver/message/router.go
@@ -0,0 +1,17 @@
+package apiserver
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func (h Handler) Router(parent *mux.Router) *mux.Router {
+	r := parent.NewRoute().Subrouter()
+
+	r.HandleFunc("/messages", h.SendMessages).Methods(http.MethodPost)
+	r.HandleFunc("/messages", h.ListMessages).Methods(http.MethodGet)
+	r.HandleFunc("/messages/:id", h.GetMessage).Methods(http.MethodGet)
+
+	return r
+}
