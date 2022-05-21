270bb0e73fb740f412e9d9a249a23fa96b067054
Edson Michaque <edson@michaque.com>
2022-05-22 00:37:49 +0200

    add handler handler functions

--- internal/server/sms/handler.go
+++ internal/server/sms/handler.go
@@ -7,11 +7,18 @@ import (
 )
 
 type Handler struct {
+	internal.Handler
 	Cache  internal.Cache
 	Logger internal.Logger
 }
 
-func (h Handler) SendMessages(rw http.ResponseWriter, r *http.Request) {}
+func (h Handler) SendMessages(rw http.ResponseWriter, r *http.Request) {
+	var req NewSMS
+
+	if err := h.ReadJSON(r.Body, &req); err != nil {
+		return
+	}
+}
 
 func (h Handler) ListMessages(rw http.ResponseWriter, r *http.Request) {}
 
---
+++ internal/server/sms/types.go
@@ -0,0 +1,9 @@
+package sms
+
+type NewSMS struct {
+	Messages []Message `json:"messages,omitempty"`
+}
+
+type Message struct {
+	Content string `json:"content,omitempty"`
+}
