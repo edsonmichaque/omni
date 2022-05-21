461da7c771d8ba701b1b18024ef8e7ce3c0750af
Edson Michaque <edson@michaque.com>
2022-05-21 22:46:17 +0200

    add handler for session

---
+++ internal/apiserver/session/handler.go
@@ -0,0 +1,11 @@
+package session
+
+import "net/http"
+
+type Handler struct{}
+
+func (h Handler) SendMessages(rw http.ResponseWriter, r *http.Request) {}
+
+func (h Handler) ListMessages(rw http.ResponseWriter, r *http.Request) {}
+
+func (h Handler) GetMessage(rw http.ResponseWriter, r *http.Request) {}
--- internal/apiserver/session/router.go
+++ internal/apiserver/session/router.go
@@ -1,4 +1,4 @@
-package message
+package session
 
 import (
 	"net/http"
